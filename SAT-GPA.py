b28af74d3f85922f9e6ac169fc2e97da272fa79a
mb5542 <michal5542@gmail.com>
2025-07-04 16:55:52 +0200

    Data load & model train

---
+++ SAT-GPA.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  4 16:38:39 2025
+
+@author: Burzynek
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# import data
+data = pd.read_csv(r'D:\Programy\Python312\Scripts\sat-gpa\high_school_sat_gpa.csv', sep=' ',usecols=['math_SAT','verb_SAT','high_GPA'])
+
+data.head()
+data.dtypes
+
+# Scatter plot showing the relationship between math_SAT and high_GPA
+plt.figure(figsize=(7,5))
+plt.scatter(data['math_SAT'], data['high_GPA'], color='b')
+plt.show()
+
+# Linear regression model lr_math that will describe the relationship between math_SAT and high_GPA
+lr_math = LinearRegression()
+lr_math.fit(X = data['math_SAT'].values.reshape(-1,1), y = data['high_GPA'].values)
